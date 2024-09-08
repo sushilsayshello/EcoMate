133a1df8e9310be9f558ac1ac33ec996751c5e4d
Sushil Shakya <sushilsayshello@gmail.com>
2024-09-08 14:40:00 +1000

    Initial Commit

---
+++ EcoMateProjectApp/EcoMateProjectAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoMateProjectAppApp.swift
+//  EcoMateProjectApp
+//
+//  Created by admin on 8/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct EcoMateProjectAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
